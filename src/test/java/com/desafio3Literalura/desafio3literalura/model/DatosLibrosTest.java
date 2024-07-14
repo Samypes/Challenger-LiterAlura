84005866ea0bde54156bc576805f2a7b7d75f061
Samypes <147286232+Samypes@users.noreply.github.com>
2024-07-14 13:22:21 -0500

    Update DatosLibrosTest.java

---
+++ src/test/java/com/desafio3Literalura/desafio3literalura/model/DatosLibrosTest.java
@@ -0,0 +1,24 @@
+package com.desafio3Literalura.desafio3literalura.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatosLibrosTest {
+
+    @Test
+    void titulo() {
+    }
+
+    @Test
+    void autor() {
+    }
+
+    @Test
+    void idiomas() {
+    }
+
+    @Test
+    void numeroDeDesacargas() {
+    }
+}
